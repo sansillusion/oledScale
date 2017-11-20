4ca3d3bad5a44af2cde1fd7621e887324cf4161e
Steve Olmstead <sansillusion@gmail.com>
2017-11-20 17:56:34 -0500

    added some smothing

--- oledScale.ino
+++ oledScale.ino
@@ -26,16 +26,9 @@ void setup() {
   u8g2.begin();
 
   Serial.begin(115200);
-  Serial.println("HX711 calibration");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
-
   EEPROM.get( 0, romread );
   if (romread == romread) {
     calibration_factor = romread;
-    Serial.println(romread);
   }
 
   scale.set_scale();
@@ -51,27 +44,33 @@ void tare() {
 void loop() {
   if (taremoi != 0) {
     Serial.println("Taring");
-    scale.tare(); //Reset the scale to 0
     u8g2.firstPage();
     do {
       u8g2.setFont(u8g2_font_logisoso28_tr);
       u8g2.drawStr(24, 45, "Tare");
     } while ( u8g2.nextPage() );
-    delay(1000);
+    delay(500);
+    scale.tare(); //Reset the scale to 0
+    delay(500);
     taremoi = 0;
   }
   scale.set_scale(calibration_factor); //Adjust to this calibration factor
   Serial.print("Reading: ");
-  float poids = scale.get_units();
+  //float poids = scale.get_units();
+  float poids = 0;
+  float temptotal = 0;
+  for (int x = 0; x < 5; x++) {
+    float rawVal = scale.get_units();
+    temptotal = temptotal + rawVal;
+  }
+  poids = temptotal / 5;
   if (poids < 0.09) {
     if (poids > -0.09) {
       poids = 0;
     }
   }
-  Serial.print(poids, 1);
+  Serial.print(poids);
   Serial.print(" Grams");
-  Serial.print(" calibration_factor: ");
-  Serial.print(calibration_factor);
   Serial.println();
   float onces = poids / 28.3495;
   char g[10];
@@ -87,7 +86,6 @@ void loop() {
     u8g2.drawStr(110, 32, "G");
     u8g2.drawStr(90, 64, "oz");
   } while ( u8g2.nextPage() );
-  delay(200);
   if (Serial.available())
   {
     char temp = Serial.read();
